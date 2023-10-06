4e85fa0445c419cca4305ff24deed9f32c8d0a3b
Guo Mingfei <guomingfei@DN0a229782.SUNet>
2023-10-06 13:32:32 -0700

    hw1

--- scl/HWs/HW1/hw1_p1.py
+++ scl/HWs/HW1/hw1_p1.py
@@ -28,8 +28,19 @@ def shubhams_fair_coin_generator():
 
     ##############################
     # ADD CODE HERE
-    raise NotImplementedError
+    # raise NotImplementedError
     ##############################
+    while 1:
+        toss_1 = biased_coin_generator()
+        num_biased_tosses += 1
+        toss_2 = biased_coin_generator()
+        num_biased_tosses += 1
+        if toss_1 + toss_2 == "HT":
+            toss = "H"
+            break
+        if toss_1 + toss_2 == "TH":
+            toss = "T"
+            break
 
     return toss, num_biased_tosses
 
@@ -68,5 +79,12 @@ def test_shubhams_fair_coin_generator():
     # 2. add test to check if avg value of num_biased_tosses matches what you expect
     # (both within a reasonable error range)
     # You can use np.testing.assert_almost_equal() to check if two numbers are close to some error margin
-    raise NotImplementedError
+    # raise NotImplementedError
     #############################################
+    np.testing.assert_almost_equal(empirical_dist_tosses.prob_dict["H"], 0.5, decimal=1)
+    np.testing.assert_almost_equal(empirical_dist_tosses.prob_dict["T"], 0.5, decimal=1)
+    
+    my_expectation_of_head = 2 * 0.8 * 0.2 / (1-(0.8**2+0.2**2))**2
+    # need to devide by 2 because num_biased_tosses_list contains both heads and tails
+    # while my_expectation_of_head only contains heads
+    np.testing.assert_almost_equal(np.mean(np.array(num_biased_tosses_list))/2, my_expectation_of_head, decimal=1)
